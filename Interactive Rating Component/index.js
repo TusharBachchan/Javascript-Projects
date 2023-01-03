276ae2d18e0d79c209c15a57a140e91a2df90f9b
Bachchan <tushar.bachchan@dxc.com>
2023-01-03 19:10:36 +0530

    Project Completed

---
+++ Interactive Rating Component/index.js
@@ -0,0 +1,33 @@
+let ratingButtons = document.getElementsByClassName("rating");
+let ratingId = "";
+let alreadyClicked = false;
+let prevelement;
+let ratingSelected = 0;
+for(let ratingButton of ratingButtons){
+    ratingButton.addEventListener("click", (e) => {
+        ratingId = e.target.id;
+        let buttonPressed = document.getElementById(ratingId);
+        buttonPressed.style.backgroundColor = "white";
+        ratingSelected = buttonPressed.getAttribute("valueEL");
+        console.log(ratingSelected)
+        // if(!alreadyClicked){
+        //     buttonPressed.style.backgroundColor = "white";
+        //     alreadyClicked = true;
+        // }
+        // else{
+        //     prevelement.style.color = "";
+        //     // buttonPressed.style.backgroundColor = "white";
+        //     // alreadyClicked = true;
+        // }
+    });
+}
+let submitButton = document.querySelector(".submit");
+let initialCard = document.querySelector(".initial-card");
+let thankCard = document.querySelector(".thank-card");
+let ratingEle = document.querySelector(".selected-rating");
+submitButton.addEventListener("click", () => {
+    initialCard.classList.add("not-visible");
+    thankCard.classList.remove("not-visible");
+    ratingEle.innerText = ratingSelected;
+});
+
