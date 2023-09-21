626eb36d3fa0a35561cfb51db9ec3f70b891b7cc
tusharbachchan <tusharbachchan@gmail.com>
2023-09-21 08:21:46 +0530

    added constraint checks

--- Password Generator/script.js
+++ Password Generator/script.js
@@ -1,6 +1,9 @@
 const slider = document.querySelector("#slider")
 const sliderVal = document.querySelector("#ch-len-val")
 const pw = document.querySelector("#newpw")
+const generateNewPasswordButton = document.querySelector("#generateNewPw");
+const includeUppercaseCheck = document.querySelector("#includeUp");
+let length = slider.value;
 sliderVal.textContent = slider.value;
 // const timeout = setTimeout(() => {
 //     pw.textContent="HI"
@@ -9,5 +12,14 @@ sliderVal.textContent = slider.value;
 
 slider.addEventListener("input", (e) => {
     sliderVal.textContent = e.target.value;
+    length = e.target.value;
     // clearTimeout(timeout)
 })
+
+generateNewPasswordButton.addEventListener("click", () => generateNewPassword())
+
+function generateNewPassword(){
+    pw.textContent = length;
+    console.log(length);
+    console.log(includeUppercaseCheck)
+}
